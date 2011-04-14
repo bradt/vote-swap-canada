dcca376856240396d42232a0777578903b22124b
Brad Touesnard <brad@touesnard.com>
2011-04-14 10:16:07 -0300

    Adding ignore.

---
+++ config.sample.php
@@ -0,0 +1,18 @@
+<?php
+$theme = 'default';
+
+// MySQL settings
+define('DB_NAME', '');     // The name of the database
+define('DB_USER', '');     // Your MySQL username
+define('DB_PASSWORD', ''); // ...and password
+define('DB_HOST', 'localhost');     // 99% chance you won't need to change this value
+
+define('DEBUG', 3);
+
+$config['name'] = 'Vote Swap Canada';
+$config['siteurl'] = 'http://voteswap.sharurl.com/voteswapcanada';
+$config['nobuffer'] = array();
+
+// Filters
+include('inc/filters.php');
+?>
